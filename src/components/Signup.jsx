ab33880d9ceb4c10e54803592c501010d9185e33
German-uruguay <106714484+German-uruguay@users.noreply.github.com>
2022-11-02 18:43:29 -0300

    cambios en el form del sign up

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,35 @@
 import "./Login-Signup.css";
 import signupLogo from "../img/logoLoginRegister.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Signup() {
+  const [firstName, setFirstName] = useState({});
+  const [lastName, setLastName] = useState({});
+  const [eMail, setEMail] = useState({});
+  const [userName, setUserName] = useState({});
+  const [password, setPassword] = useState({});
+  const [avatar, setAvatar] = useState({});
+
+  const createUser = async () => {
+    const response = await axios({
+      method: "POST",
+      url: "localhost:8000/signup",
+      data: {
+        firstname: firstName,
+        lastname: lastName,
+        username: eMail,
+        email: userName,
+        password: password,
+        avatar: avatar,
+      },
+      params: {
+        // Aquí se pueden pasar parámetros que irían en el query string.
+      },
+    });
+  };
+
   return (
     <main id="background">
       <div className="container">
@@ -11,7 +39,13 @@ function Signup() {
             <h2 className="margin-aligment">Hi! Welcome to Twitter Clone 👋</h2>
           </div>
           <div id="right" className="col bg-white">
-            <form className="formRegister">
+            <form
+              className="formRegister"
+              method="post"
+              onSubmit={(e) => {
+                createUser(), e.preventDefault();
+              }}
+            >
               <h2>Sign up</h2>
               <small>Create and account and star using Twitter</small>
               <div className="mb-1">
@@ -22,6 +56,7 @@ function Signup() {
                   id="firstname"
                   name="firstname"
                   placeholder="First name"
+                  onChange={(event) => setFirstName(event.target.value)}
                 />
               </div>
               <div className="mb-1">
@@ -32,6 +67,7 @@ function Signup() {
                   id="lastname"
                   name="lastname"
                   placeholder="Last name"
+                  onChange={(event) => setLastName(event.target.value)}
                 />
               </div>
               <div className="mb-1">
@@ -42,6 +78,7 @@ function Signup() {
                   id="email"
                   name="email"
                   placeholder="Email"
+                  onChange={(event) => setEMail(event.target.value)}
                 />
               </div>
               <div className="mb-1">
@@ -52,6 +89,7 @@ function Signup() {
                   id="username"
                   name="username"
                   placeholder="Username"
+                  onChange={(event) => setUserName(event.target.value)}
                 />
               </div>
 
@@ -63,6 +101,7 @@ function Signup() {
                   id="avatar"
                   name="avatar"
                   placeholder="Choose file..."
+                  onChange={(event) => setAvatar(event.target.value)}
                 />
               </div>
               <div className="mb-1">
@@ -73,6 +112,7 @@ function Signup() {
                   id="password"
                   name="password"
                   placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
                 />
               </div>
               <div className="mb-1">
