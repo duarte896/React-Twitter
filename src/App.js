aa66f9251449fdb9726b609c1ceff4b56b56a7d5
Luciano Rodriguez <rodriguez.luciano.augusto@gmail.com>
2022-11-02 14:12:12 -0300

    Rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Home />} />
+        <Route path="/signup" element={<Home />} />
+      </Routes>
     </div>
   );
 }
